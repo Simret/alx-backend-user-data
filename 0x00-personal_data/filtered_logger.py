6710b39d4a4baa5d97d3576c7d8be30001a40694
simret <simruineb@gmail.com>
2023-03-03 00:10:32 +0300

    Indentation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -35,12 +35,13 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
-    def get_logger() -> logging.Logger:
-        '''Implementing a logger'''
-        logger = logging.getLogger("user_data")
-        logger.setLevel(logging.INFO)
-        logger.propagate = False
-        handler = logging.StreamHandler()
-        handler.setFormatter(RedactingFormatter(PII_FIELDS))
-        logger.addHandler(handler)
-        return logger
+
+def get_logger() -> logging.Logger:
+    '''Implementing a logger'''
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(handler)
+    return logger
