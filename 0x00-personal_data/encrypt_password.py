0fd65dd4110a6644db0f1a76d6c86e5a89f27947
simret <simruineb@gmail.com>
2023-03-03 01:47:02 +0300

    Password check

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,7 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     '''Password generation'''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Checking if password matches'''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
