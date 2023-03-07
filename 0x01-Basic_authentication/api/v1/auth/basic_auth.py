a5074742a6b61a9d657a8e0ea447f4bfa8f607a1
simret <simruineb@gmail.com>
2023-03-08 00:48:10 +0300

    Base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 '''BasicAuth class'''
+import re
 from .auth import Auth
 
 
 class BasicAuth(Auth):
     '''Implementation of Basic Authorization'''
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        '''Returning the Base64 part of the Authorization header'''
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
+        token = authorization_header.split(" ")[-1]
+        return token
