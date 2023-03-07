b45a502485e006858eb966a704202443a619c5b5
simret <simruineb@gmail.com>
2023-03-07 20:23:51 +0300

    Exclude paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,15 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''Determining whether a given path requires authentication or not'''
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        path = path + '/' if path[-1] != '/' else path
+        if path not in excluded_paths:
+            return True
+        else:
+            return False
 
     def authorization_header(self, request=None) -> str:
         '''Returning the authorization header from a request object'''
