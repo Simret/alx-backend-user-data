df06bfc94b66064e52c7dd9fa99b0615627e1d57
simret <simruineb@gmail.com>
2023-03-08 04:28:58 +0300

    Generate password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,8 @@
 import re
 import base64
 from .auth import Auth
-from typing import TypeVar
+from typing import TypeVar, Tuple
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -12,7 +13,7 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         '''Returning the Base64 part of the Authorization header'''
-        if type(authorization_header) == str:
+        if isinstance(authorization_header, str):
             pattern = r'Basic (?P<token>.+)'
             field_match = re.fullmatch(pattern, authorization_header.strip())
             if field_match is not None:
@@ -28,7 +29,7 @@ class BasicAuth(Auth):
         Decode a Base64-encoded string
         """
         if base64_authorization_header is None or \
-           type(base64_authorization_header) != str:
+           not isinstance(base64_authorization_header, str):
             return None
 
         try:
@@ -36,3 +37,22 @@ class BasicAuth(Auth):
                 decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str,
+    ) -> Tuple[str, str]:
+        """Extracts user credentials from a base64-decoded authorization
+        header that uses the Basic authentication flow.
+        """
+        if isinstance(decoded_base64_authorization_header, str):
+            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            field_match = re.fullmatch(
+                pattern,
+                decoded_base64_authorization_header.strip(),
+            )
+            if field_match is not None:
+                user = field_match.group('user')
+                password = field_match.group('password')
+                return user, password
+        return None, None
