35b1066f5cf8b7cc5c6609eff5345771b03f6ea7
simret <simruineb@gmail.com>
2023-03-09 18:38:14 +0300

    Create session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 """SessionAuth"""
+from typing import Dict
 from .auth import Auth
+import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
     """Implementing Session Authorization"""
+    user_id_by_session_id: Dict[str, str] = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creating a session id for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
