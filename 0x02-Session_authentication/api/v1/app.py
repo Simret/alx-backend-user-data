24cc059e1cf8de10369697bdbc2bd12b68fb0502
simret <simruineb@gmail.com>
2023-03-09 19:35:08 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,9 +34,11 @@ def bef_req():
         excluded = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded):
+            cookie = auth.session_cookie(request)
             user = auth.current_user(request)
             if auth.authorization_header(request) is None:
                 abort(401)
