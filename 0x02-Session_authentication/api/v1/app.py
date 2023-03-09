8ded11c0b1879b0970f80724ea1830eebc11266c
simret <simruineb@gmail.com>
2023-03-09 19:42:46 +0300

    Fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,9 +38,8 @@ def bef_req():
             '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded):
-            cookie = auth.session_cookie(request)
             user = auth.current_user(request)
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
