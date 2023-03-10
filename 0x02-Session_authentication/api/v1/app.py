922cd3109310d2ec5ae433d5ef2c601ad56ae32b
simret <simruineb@gmail.com>
2023-03-10 05:33:14 +0300

    Update app~

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,6 +10,8 @@ import os
 
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -26,6 +28,8 @@ elif AUTH_TYPE == "session_auth":
     auth = SessionAuth()
 elif AUTH_TYPE == 'session_exp_auth':
     auth = SessionExpAuth()
+if AUTH_TYPE == 'session_db_auth':
+    auth = SessionDBAuth()
 
 
 @app.before_request
