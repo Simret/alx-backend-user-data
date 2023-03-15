e99d03b7d1e3f377a4983aef4fe796bf976d378b
simret <simruineb@gmail.com>
2023-03-15 17:45:53 +0300

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env python3
+"""Auth Class for user attributes validation
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from db import DB
+from user import User
+import bcrypt
+
+
+def _hash_password(password: str) -> str:
+    """Takes in password string argument
+    Returns bytes
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
