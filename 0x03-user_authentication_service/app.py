886c6a3da1ae51b2f3948987681385ac99261701
simret <simruineb@gmail.com>
2023-03-16 19:44:26 +0300

    Loginx

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """ Flask App class
 """
 from auth import Auth
-from flask import Flask, jsonify, request, redirect
+from flask import Flask, jsonify, request, abort, redirect
 
 AUTH = Auth()
 app = Flask(__name__)
@@ -31,3 +31,23 @@ def user() -> str:
     except Exception:
         return jsonify({"message": "email already registered"}), 400
     
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """
+    Log in a user if the credentials provided are correct, and create a new
+    session for them.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    resp = jsonify({"email": f"{email}", "message": "logged in"})
+    resp.set_cookie("session_id", session_id)
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
