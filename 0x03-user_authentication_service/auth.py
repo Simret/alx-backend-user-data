11fe58a4e458c65a138ed6a91dc12c4d22b23372
simret <simruineb@gmail.com>
2023-03-16 19:49:03 +0300

    Find user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,15 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """
+        get_user_from_session_id.
+        """
+        if session_id is None:
+            return
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user.email
+        except NoResultFound:
+            return
