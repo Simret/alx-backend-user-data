09e8ae9c42299db92fc6ed015b4f080ae869aa48
simret <simruineb@gmail.com>
2023-03-16 23:17:36 +0300

    Logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -30,7 +30,8 @@ def user() -> str:
         return jsonify({"email": f"{email}", "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
-    
+
+
 @app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login() -> str:
     """
@@ -47,6 +48,19 @@ def login() -> str:
     resp = jsonify({"email": f"{email}", "message": "logged in"})
     resp.set_cookie("session_id", session_id)
     return resp
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    """
+    Log out a logged in user and destroy their session
+    """
+    session_id = request.cookies.get("session_id", None)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None or session_id is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
