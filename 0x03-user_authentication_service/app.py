eef4a0275fb2e51393235fddc3009cfd9cb43ed8
simret <simruineb@gmail.com>
2023-03-17 04:41:35 +0300

    Update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -106,7 +106,21 @@ def get_reset_password_token() -> str:
         return jsonify({"email": f"{email}", "reset_token": f"{token}"})
 
 
-
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """PUT /reset_password
+    Return:
+      - message
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_psw = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, new_psw)
+        return (jsonify({"email": f"{email}", "message": "Password updated"}),
+                200)
+    except Exception:
+        abort(403)
 
 
 if __name__ == "__main__":
